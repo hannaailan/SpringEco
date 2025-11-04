27fa8e295ecdda40e68e4d9858f2706889bfb7f9
hannaailan <126789675+hannaailan@users.noreply.github.com>
2025-11-04 13:26:24 +0100

    Add files via upload

---
+++ springEcom/src/main/java/com/codeqithmoch/SpringEcom/SpringEcoApplication.java
@@ -0,0 +1,13 @@
+package com.codeqithmoch.SpringEcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcoApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringEcoApplication.class, args);
+        System.out.println("hkjlm");
+    }
+}
